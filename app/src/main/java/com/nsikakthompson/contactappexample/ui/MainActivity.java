419c7f61c4f8ffa0358eaaa2591540033ee83fc5
Nsikaktopdown <nsikakthompson73@gmail.com>
2018-02-17 09:12:45 +0100

    implement search

--- app/src/main/java/com/nsikakthompson/contactappexample/ui/MainActivity.java
+++ app/src/main/java/com/nsikakthompson/contactappexample/ui/MainActivity.java
@@ -4,10 +4,12 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -34,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     PersonDatabase personDatabase;
     private RecyclerAdapter contactAdapter;
     private RecyclerView recyclerView;
+    String query;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,10 +67,10 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setNestedScrollingEnabled(false);
 
-        contactAdapter = new RecyclerAdapter(this,new ArrayList<>());
+        contactAdapter = new RecyclerAdapter(this, new ArrayList<>());
         recyclerView.setAdapter(contactAdapter);
 
-        mainAcivityViewmodel.getContact().observe(this, contacts ->{
+        mainAcivityViewmodel.getContact().observe(this, contacts -> {
             contactAdapter.setItems(contacts);
             contactAdapter.notifyDataSetChanged();
         });
@@ -76,11 +79,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem search = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(search);
+        search(searchView);
         return true;
     }
 
@@ -103,5 +108,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
+    }
+
+    private void search(SearchView searchView) {
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                query = newText;
+
+                if (contactAdapter != null) {
+                    contactAdapter.getFilter().filter(newText);
+                    contactAdapter.notifyDataSetChanged();
+                }
+
+
+                return true;
+            }
+        });
     }
 }
--- app/src/main/java/com/nsikakthompson/contactappexample/ui/RecyclerAdapter.java
+++ app/src/main/java/com/nsikakthompson/contactappexample/ui/RecyclerAdapter.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,44 +14,28 @@ import com.nsikakthompson.contactappexample.R;
 import com.nsikakthompson.contactappexample.data.Person;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Nsikak on 2/16/18.
  */
 
-public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> implements Filterable {
 
-    private List<Person> contactList;
+    private ArrayList<Person> contactList = new ArrayList<>();
+    private ArrayList<Person> mFilteredList;
+
     private Context context;
     private View.OnClickListener deleteClickListener;
     private View.OnClickListener viewClickListener;
 
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-
-        TextView mContactName_txt, mContactPhone_txt;
-        ImageView mPhoto;
-
-        public MyViewHolder(View view) {
-            super(view);
-
-           mContactName_txt = (TextView) view.findViewById(R.id.contact_name);
-           mContactPhone_txt = (TextView) view.findViewById(R.id.contact_phone);
-           mPhoto = itemView.findViewById(R.id.contact_thumb);
-
-
-        }
-
-    }
-
-
-    public RecyclerAdapter(Context mContext, List<Person> contactList) {
+    public RecyclerAdapter(Context mContext, ArrayList<Person> contactList) {
 
         this.context = mContext;
         this.contactList = contactList;
-        //this.deleteClickListener = deleteClickListener;
-        //this.viewClickListener = viewClickListener;
+        this.mFilteredList = contactList;
 
     }
 
@@ -73,13 +59,71 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
 
     }
 
+
     @Override
     public int getItemCount() {
         return contactList.size();
     }
 
     public void setItems(List<Person> contacts) {
-        this.contactList = contacts;
+        this.contactList = (ArrayList<Person>) contacts;
+        this.mFilteredList  = (ArrayList<Person>) contacts;
         notifyDataSetChanged();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+
+                String charString = charSequence.toString();
+
+                if (charString.isEmpty()) {
+
+                    mFilteredList = contactList;
+                } else {
+
+                    ArrayList<Person> filteredList = new ArrayList<>();
+
+                    for (Person contact: contactList) {
+
+                        if (contact.getContactName().toLowerCase().contains(charString) || contact.getContactPhone().toLowerCase().contains(charString)) {
+
+                            filteredList.add(contact);
+                        }
+                    }
+
+                    mFilteredList = filteredList;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = mFilteredList;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                mFilteredList = (ArrayList<Person>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView mContactName_txt, mContactPhone_txt;
+        ImageView mPhoto;
+
+        public MyViewHolder(View view) {
+            super(view);
+
+            mContactName_txt = (TextView) view.findViewById(R.id.contact_name);
+            mContactPhone_txt = (TextView) view.findViewById(R.id.contact_phone);
+            mPhoto = itemView.findViewById(R.id.contact_thumb);
+
+
+        }
+
     }
 }
